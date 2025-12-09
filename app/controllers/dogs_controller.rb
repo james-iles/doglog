1eabc4bd9941bbaaab6b33d76a64bf9db08d7f76
Joao Dias <joao107dias@gmail.com>
2025-12-09 15:59:39 +0000

    commit fo the pull

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,22 +1,51 @@
 class DogsController < ApplicationController
   def index
+    @household = Household.find(params[:household_id])
+    @dogs = @household.dogs
   end
 
   def show
+    @dog = Dog.find(params[:id])
   end
 
   def new
+    @household = Household.find(params[:household_id])
+    @dog = @household.dogs.new
   end
 
   def create
+    @household = Household.find(params[:household_id])
+    @dog = @household.dogs.new(dog_params)
+    if @dog.save
+      redirect_to dog_path(@dog), notice: "Dog created successfully."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
+    @dog = Dog.find(params[:id])
   end
 
   def update
+    @dog = Dog.find(params[:id])
+    if @dog.update(dog_params)
+      redirect_to dog_path(@dog), notice: "Dog updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
-  def delete
+  def destroy
+    @dog = Dog.find(params[:id])
+    @household = @dog.household
+    @dog.destroy
+    redirect_to household_path(@household), notice: "Dog deleted."
+  end
+
+  private
+
+  def dog_params
+    params.require(:dog).permit(:name, :breed, :dob)
   end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,3 +1,12 @@
 class Dog < ApplicationRecord
   belongs_to :household
+
+  has_many :documents, dependent: :destroy
+  has_many :appointments, dependent: :destroy
+  has_many :chats, dependent: :destroy
+
+  validates :name, presence: true,  length: { minimum: 2, maximum: 10 }
+  validates :breed, presence: true
+  validates :dob, presence: true
+  validates :household_id, presence: true
 end
