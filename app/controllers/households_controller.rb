2455fe65ae09c719a4eb4038381ffbbdbbf2ae34
FerizatJussupbekova ferizat12@gmail.com <ferizat12@gmail.com>
2025-12-09 16:52:05 +0100

    working on household model

--- app/controllers/households_controller.rb
+++ app/controllers/households_controller.rb
@@ -1,13 +1,39 @@
 class HouseholdsController < ApplicationController
-  def show
+  skip_before_action :authenticate_user!, only: [:new, :create]
+  before_action :redirect_if_signed_in, only: [:new, :create]
+
+  def new
+    @household = Household.new
   end
 
   def create
+    @household = Household.new(household_params)
+
+    if @household.save
+      session[:household_id] = @household.id
+      redirect_to new_user_registration_path
+    else
+      render :new
+    end
   end
 
-  def delete
+
+  def destroy
+    @household = Household.find(params[:id])
+    @household = Household.destroy
+    redirect_to root_path
   end
 
   def update
+    @household = Household.find(params[:id])
+    @household.update(params[household_params])
+    redirect_to root_path
+  end
+end
+
+private
+
+  def household_params
+    params.require(:household).permit(:name)
   end
 end
