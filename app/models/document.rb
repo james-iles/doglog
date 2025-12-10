97b268450ca77e938df3055669c99f323e9edbd0
FerizatJussupbekova ferizat12@gmail.com <ferizat12@gmail.com>
2025-12-10 18:10:17 +0100

    added appointment to the category of docs

--- app/models/document.rb
+++ app/models/document.rb
@@ -2,7 +2,7 @@ class Document < ApplicationRecord
   belongs_to :dog
   has_many_attached :photos
 
-  CATEGORIES = ["Medical", "Training", "Grooming", "Nutrition", "Behavior", "Other"]
+  CATEGORIES = ["Medical", "Training", "Grooming", "Nutrition", "Behavior", "Appointment", "Other"]
 
   validates :title, :content, :category, presence: true
   validates :category, inclusion: { in: CATEGORIES }
