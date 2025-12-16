26608436f485e6fe9d21c5c7d28a4e2cf727efc1
James Iles <jamesadiles@gmail.com>
2025-12-16 11:28:47 +0000

    Updated fonts and added document copy across profiles for household

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -19,7 +19,13 @@ class DocumentsController < ApplicationController
     @document = @dog.documents.new(document_params)
 
     if @document.save
-      redirect_to @document, notice: 'Document created successfully'
+      copied_count = copy_to_other_dogs(@document)
+      notice_message = if copied_count > 0
+        "Created and copied to #{copied_count} other #{'dog'.pluralize(copied_count)}"
+      else
+        'Document created'
+      end
+      redirect_to @document, notice: notice_message
     else
       render :new
     end
@@ -42,7 +48,30 @@ class DocumentsController < ApplicationController
     redirect_to dog_documents_path(@document.dog), notice: "Document deleted"
   end
 private
+
   def document_params
     params.require(:document).permit(:title, :content, :category, photos:[])
+  end
+
+  def copy_to_other_dogs(original_document)
+    return 0 unless params[:copy_to_dog_ids].present?
+
+    dog_ids = params[:copy_to_dog_ids]
+    # Only allow dogs from same household for security
+    valid_dogs = @dog.household.dogs.where(id: dog_ids).where.not(id: @dog.id)
+
+    valid_dogs.each do |dog|
+      new_doc = dog.documents.create(
+        title: original_document.title,
+        content: original_document.content,
+        category: original_document.category
+      )
+      # Copy photos by attaching the same blobs (efficient, no duplicate storage)
+      original_document.photos.each do |photo|
+        new_doc.photos.attach(photo.blob)
+      end
+    end
+
+    valid_dogs.count
   end
 end
