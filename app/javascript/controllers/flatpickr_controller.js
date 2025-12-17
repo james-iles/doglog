5d435bab878f109f35eb941313d6e7d06d2414e2
Joao Dias <joao107dias@gmail.com>
2025-12-17 15:20:53 +0000

    done

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,16 +1,35 @@
 import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"
-import rangePlugin from "flatpickrRangePlugin";
 
 export default class extends Controller {
-  static targets = [ 'startDateInput', 'endDateInput' ]
+  static targets = ["startDateInput", "endDateInput"]
 
   connect() {
-    flatpickr(this.startDateInputTarget, {
-      enableTime: true,        // Enable time selection
-      dateFormat: "Y-m-d H:i", // Format: 2024-12-16 14:30
-      time_24hr: true,         // Use 24-hour format
-      "plugins": [new rangePlugin({ input: this.endDateInputTarget})]
+    // Start date picker
+    this.startPicker = flatpickr(this.startDateInputTarget, {
+      enableTime: true,
+      dateFormat: "Y-m-d H:i",
+      time_24hr: true,
+      onChange: (selectedDates) => {
+        if (selectedDates.length > 0) {
+          // Allow end date, but not before start date
+          this.endPicker.set("minDate", selectedDates[0])
+
+          // Optional: auto-fill end date if empty (+30 min)
+          if (!this.endDateInputTarget.value) {
+            const endDate = new Date(selectedDates[0].getTime() + 30 * 60000)
+            this.endPicker.setDate(endDate)
+          }
+        }
+      }
+    })
+
+    // End date picker
+    this.endPicker = flatpickr(this.endDateInputTarget, {
+      enableTime: true,
+      dateFormat: "Y-m-d H:i",
+      time_24hr: true,
+      allowInput: true
     })
   }
 }
